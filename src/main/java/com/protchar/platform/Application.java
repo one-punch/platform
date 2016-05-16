63b40e590a46ee0057cbae83b4271cdc71286cff
benko <benko@3cps.ca>
2016-05-16 12:13:50 +0800

    add asset

--- src/main/java/com/protchar/platform/Application.java
+++ src/main/java/com/protchar/platform/Application.java
@@ -8,14 +8,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
         DatabaseConfig.buildDatabaseConfig();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/public/**")) {
+            registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
+        }
     }
 }
