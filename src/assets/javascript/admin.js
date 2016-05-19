3b907f4d721c7cd4f8742f9b1f4f37154cbc33b6
scalaview <chailink100@gmail.com>
2016-05-19 22:51:44 +0800

    fix js

--- src/assets/javascript/admin.js
+++ src/assets/javascript/admin.js
@@ -2,7 +2,7 @@
 //= require /jquery/jquery.min.js
 //= require /jquery-migrate/jquery-migrate.min.js
 //= require /bootstrap/js/bootstrap.min.js
-//= require /spin/javascript/spin.min.js
+//= require /spin/javascripts/jquery.spin.js
 //= require /autosize/autosize.min.js
 //= require /jquery-knob/jquery.knob.min.js
 //= require /rickshaw/rickshaw.min.js
